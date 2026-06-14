96a4c7b01a62f2bff0c59a5e01914b289b1d0686
nazzzzdza <nnnarrytta@gmail.com>
2026-06-14 15:14:38 +0200

    Update index.js

--- index.js
+++ index.js
@@ -11,9 +11,7 @@ const {
 
 const app = express();
 app.get("/", (_, res) => res.send("Bot online"));
-app.listen(process.env.PORT || 3000, () =>
-  console.log("Web server running")
-);
+app.listen(process.env.PORT || 3000, () => console.log("Web server running"));
 
 const client = new Client({
   intents: [GatewayIntentBits.Guilds],
@@ -23,53 +21,31 @@ const client = new Client({
 client.commands = new Map();
 const commands = [];
 
-try {
-  const cmdPath = path.join(__dirname, "commands");
+// load commands
+const cmdPath = path.join(__dirname, "commands");
+for (const file of fs.readdirSync(cmdPath).filter(f => f.endsWith(".js"))) {
+  const cmd = require(`./commands/${file}`);
+  client.commands.set(cmd.data.name, cmd);
+  commands.push(cmd.data.toJSON());
+}
 
-  if (fs.existsSync(cmdPath)) {
-    for (const file of fs.readdirSync(cmdPath).filter(f => f.endsWith(".js"))) {
-      const cmd = require(`./commands/${file}`);
-      client.commands.set(cmd.data.name, cmd);
-      commands.push(cmd.data.toJSON());
-    }
+// load events
+const eventPath = path.join(__dirname, "events");
+for (const file of fs.readdirSync(eventPath).filter(f => f.endsWith(".js"))) {
+  const event = require(`./events/${file}`);
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args, client));
   } else {
-    console.log("No commands folder found");
+    client.on(event.name, (...args) => event.execute(...args, client));
   }
-
-} catch (err) {
-  console.error("Command load error:", err);
 }
 
-try {
-  const eventPath = path.join(__dirname, "events");
-
-  if (fs.existsSync(eventPath)) {
-    for (const file of fs.readdirSync(eventPath).filter(f => f.endsWith(".js"))) {
-      const event = require(`./events/${file}`);
-
-      if (event.once) {
-        client.once(event.name, (...args) => event.execute(...args, client));
-      } else {
-        client.on(event.name, (...args) => event.execute(...args, client));
-      }
-    }
-  }
-} catch (err) {
-  console.error("Event load error:", err);
-}
-
+// deploy + login
 client.once("ready", async () => {
   console.log(`Logged in as ${client.user.tag}`);
 
-  client.user.setPresence({
-    activities: [{ name: "tickets & queue", type: 3 }],
-    status: "online"
-  });
-
   try {
     const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
-
-    console.log("Deploying slash commands...");
 
     await rest.put(
       Routes.applicationGuildCommands(
