703550dcf350c8d5a58e384cfb5cf477ec356d3f
nazzzzdza <nnnarrytta@gmail.com>
2026-06-14 11:30:41 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+const express = require("express");
+const app = express();
 
+app.get("/", (_, res) => {
+  res.send("Queue Ticket Bot Running");
+});
+
+app.listen(3000, () => {
+  console.log("Web server running");
+});
