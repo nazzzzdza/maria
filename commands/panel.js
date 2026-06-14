96b29916f5d34851a99b2ab3d25a1ebca5e8fac4
nazzzzdza <nnnarrytta@gmail.com>
2026-06-14 14:50:40 +0200

    Update panel.js

--- commands/panel.js
+++ commands/panel.js
@@ -1,11 +1,27 @@
-const { SlashCommandBuilder } = require("discord.js");
+const { SlashCommandBuilder, ActionRowBuilder, StringSelectMenuBuilder } = require("discord.js");
 
 module.exports = {
-  data: new SlashCommandBuilder()
-    .setName("panel")
-    .setDescription("Open ticket panel"),
+data: new SlashCommandBuilder()
+.setName("panel")
+.setDescription("Open ticket panel"),
 
-  async execute(interaction) {
-    await interaction.reply("Panel works");
-  }
+async execute(interaction) {
+const row = new ActionRowBuilder().addComponents(
+new StringSelectMenuBuilder()
+.setCustomId("ticket_select")
+.setPlaceholder("Open a ticket")
+.addOptions([
+{ label: "Buying", value: "buying" },
+{ label: "Linking", value: "linking" }
+])
+);
+
+```
+await interaction.reply({
+  content: "Ticket panel",
+  components: [row]
+});
+```
+
+}
 };
