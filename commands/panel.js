c6542b3db980121911c89178b22b62dcd5970d32
nazzzzdza <nnnarrytta@gmail.com>
2026-06-14 18:52:57 +0200

    Update panel.js

--- commands/panel.js
+++ commands/panel.js
@@ -1,35 +1,39 @@
 const {
-  SlashCommandBuilder,
-  ActionRowBuilder,
-  StringSelectMenuBuilder,
-  EmbedBuilder
+SlashCommandBuilder,
+ActionRowBuilder,
+StringSelectMenuBuilder
 } = require("discord.js");
 
 module.exports = {
-  data: new SlashCommandBuilder()
-    .setName("panel")
-    .setDescription("Open ticket panel"),
+data: new SlashCommandBuilder()
+.setName("panel")
+.setDescription("Open ticket panel"),
 
-  async execute(interaction) {
+async execute(interaction) {
 
-    const embed = new EmbedBuilder()
-      .setTitle("🎫 Ticket Panel")
-      .setDescription("Select a category below to open a ticket")
-      .setColor(0x2b2d31);
+const row = new ActionRowBuilder().addComponents(
+  new StringSelectMenuBuilder()
+    .setCustomId("ticket_select")
+    .setPlaceholder("Open a ticket")
+    .addOptions(
+      {
+        label: "Buying",
+        value: "buying"
+      },
+      {
+        label: "Linking",
+        value: "linking"
+      }
+    )
+);
 
-    const row = new ActionRowBuilder().addComponents(
-      new StringSelectMenuBuilder()
-        .setCustomId("ticket_select")
-        .setPlaceholder("Choose a ticket type")
-        .addOptions(
-          { label: "Buying", value: "buying" },
-          { label: "Linking", value: "linking" }
-        )
-    );
+return interaction.reply({
+  content:
 
-    return interaction.reply({
-      embeds: [embed],
-      components: [row]
-    });
-  }
+`.　  ✦　  ˚  　⊹  　˖　  ❜
+      (˶•ᴗ•)⌒)ↄ   ❥୧    our   tickets _ _      ˚❒ ⠀ ⤷☒⠀⠀𖥨🍓    open  4  buying  or  linking
+.　  ✦　  ˚  　⊹  　˖　  ❜`,
+components: [row]
+});
+}
 };
