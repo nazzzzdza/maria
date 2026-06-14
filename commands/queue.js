c509fb5d2f432d34f6f3f2062ddafe9d7dfb2a80
nazzzzdza <nnnarrytta@gmail.com>
2026-06-14 14:50:58 +0200

    Update queue.js

--- commands/queue.js
+++ commands/queue.js
@@ -1,11 +1,11 @@
 const { SlashCommandBuilder } = require("discord.js");
 
 module.exports = {
-  data: new SlashCommandBuilder()
-    .setName("queue")
-    .setDescription("Create queue entry"),
+data: new SlashCommandBuilder()
+.setName("queue")
+.setDescription("Create queue entry"),
 
-  async execute(interaction) {
-    await interaction.reply("Queue works");
-  }
+async execute(interaction) {
+await interaction.reply("Queue system active (wire UI next step)");
+}
 };
