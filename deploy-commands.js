93f5727bfe3d5ff28b5ea726a5be9992541540f2
nazzzzdza <nnnarrytta@gmail.com>
2026-06-14 14:38:31 +0200

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -1,77 +1,34 @@
-const { REST, Routes, SlashCommandBuilder } = require("discord.js");
+const { REST, Routes } = require("discord.js");
+const fs = require("fs");
 
-const commands = [
-new SlashCommandBuilder()
-.setName("panel")
-.setDescription("Send the ticket panel"),
+const commands = [];
 
-new SlashCommandBuilder()
-.setName("queue")
-.setDescription("Create a queue entry")
-.addUserOption(option =>
-option
-.setName("user")
-.setDescription("Customer")
-.setRequired(true)
-)
-.addChannelOption(option =>
-option
-.setName("ticket")
-.setDescription("Ticket channel")
-.setRequired(true)
-)
-.addStringOption(option =>
-option
-.setName("buying")
-.setDescription("What they bought")
-.setRequired(true)
-)
-.addStringOption(option =>
-option
-.setName("theme")
-.setDescription("Theme")
-.setRequired(true)
-)
-.addStringOption(option =>
-option
-.setName("style")
-.setDescription("Style")
-.setRequired(true)
-)
-.addStringOption(option =>
-option
-.setName("mop")
-.setDescription("Method of payment")
-.setRequired(true)
-)
-.addStringOption(option =>
-option
-.setName("notes")
-.setDescription("Notes")
-.setRequired(true)
-)
-].map(command => command.toJSON());
+const commandFiles = fs.readdirSync("./commands").filter(f => f.endsWith(".js"));
 
-const rest = new REST({
-version: "10"
-}).setToken(process.env.TOKEN);
+for (const file of commandFiles) {
+const command = require(`./commands/${file}`);
+commands.push(command.data.toJSON());
+}
+
+const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
 
 (async () => {
 try {
-console.log("Registering commands...");
+console.log("Deploying slash commands...");
 
 ```
 await rest.put(
-  Routes.applicationCommands(process.env.CLIENT_ID),
-  {
-    body: commands
-  }
+  Routes.applicationGuildCommands(
+    process.env.CLIENT_ID,
+    process.env.GUILD_ID
+  ),
+  { body: commands }
 );
 
-console.log("Commands registered.");
+console.log("Slash commands deployed successfully");
 ```
 
-} catch (error) {
-console.error(error);
+} catch (err) {
+console.error(err);
 }
 })();
