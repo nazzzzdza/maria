c51ec8f7235e601322e6c55ac0e6ea2b94b79367
nazzzzdza <nnnarrytta@gmail.com>
2026-06-14 14:43:46 +0200

    Update deploy-commands.js

--- deploy-commands.js
+++ deploy-commands.js
@@ -3,32 +3,29 @@ const fs = require("fs");
 
 const commands = [];
 
-const commandFiles = fs.readdirSync("./commands").filter(f => f.endsWith(".js"));
+const files = fs.readdirSync("./commands").filter(f => f.endsWith(".js"));
 
-for (const file of commandFiles) {
-const command = require(`./commands/${file}`);
-commands.push(command.data.toJSON());
+for (const file of files) {
+  const cmd = require(`./commands/${file}`);
+  commands.push(cmd.data.toJSON());
 }
 
 const rest = new REST({ version: "10" }).setToken(process.env.TOKEN);
 
 (async () => {
-try {
-console.log("Deploying slash commands...");
+  try {
+    console.log("Deploying commands...");
 
-```
-await rest.put(
-  Routes.applicationGuildCommands(
-    process.env.CLIENT_ID,
-    process.env.GUILD_ID
-  ),
-  { body: commands }
-);
+    await rest.put(
+      Routes.applicationGuildCommands(
+        process.env.CLIENT_ID,
+        process.env.GUILD_ID
+      ),
+      { body: commands }
+    );
 
-console.log("Slash commands deployed successfully");
-```
-
-} catch (err) {
-console.error(err);
-}
+    console.log("Commands deployed");
+  } catch (err) {
+    console.error(err);
+  }
 })();
